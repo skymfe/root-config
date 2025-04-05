37a2c7f891624d5c003d7d2d06f5d11edb61f77e
gabrielmendes98 <gabrielmssantiago@gmail.com>
2025-04-05 17:03:44 -0300

    feat: add flight booking microfrontend and update imports in index.ejs

--- src/skymfe-root-config.ts
+++ src/skymfe-root-config.ts
@@ -10,6 +10,16 @@ registerApplication({
   activeWhen: ["/"],
 });
 
+registerApplication({
+  name: "@skymfe/flight-booking-mf",
+  app: () =>
+    import(
+      /* webpackIgnore: true */ // @ts-ignore-next
+      "@skymfe/flight-booking-mf"
+    ),
+  activeWhen: ["/flights"],
+});
+
 start({
   urlRerouteOnly: true,
 });
