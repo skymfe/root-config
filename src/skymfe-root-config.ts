cc921da14b70fed4b712f80f8234a73a05a03baa
gabrielmendes98 <gabrielmssantiago@gmail.com>
2025-04-12 18:55:06 -0300

    refactor: remove inline layout from index.ejs and utilize external layout configuration for single-spa routes

--- src/skymfe-root-config.ts
+++ src/skymfe-root-config.ts
@@ -1,15 +1,8 @@
 import { registerApplication, start } from "single-spa";
 import { constructApplications, constructLayoutEngine, constructRoutes } from "single-spa-layout";
+import layout from "./applications.json";
 
-const routes = constructRoutes(document.querySelector("#single-spa-layout"), {
-  loaders: {
-    header: "<h1>Loading header</h1>",
-  },
-  errors: {
-    header: "<h1>Failed to load header</h1>",
-  },
-  props: {},
-});
+const routes = constructRoutes(layout);
 
 const applications = constructApplications({
   routes,
