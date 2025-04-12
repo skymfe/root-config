fe65b98b1a2f9c7f13eb9e001f5bece824d0e229
gabrielmendes98 <gabrielmssantiago@gmail.com>
2025-04-12 17:17:39 -0300

    feat: enhance root config with single-spa layout and update index.ejs for new microfrontend structure

--- src/skymfe-root-config.ts
+++ src/skymfe-root-config.ts
@@ -1,25 +1,28 @@
 import { registerApplication, start } from "single-spa";
+import { constructApplications, constructLayoutEngine, constructRoutes } from "single-spa-layout";
 
-registerApplication({
-  name: "@skymfe/header-mf",
-  app: () =>
-    import(
-      /* webpackIgnore: true */ // @ts-ignore-next
-      "@skymfe/header-mf"
-    ),
-  activeWhen: ["/"],
+const routes = constructRoutes(document.querySelector("#single-spa-layout"), {
+  loaders: {
+    header: "<h1>Loading header</h1>",
+  },
+  errors: {
+    header: "<h1>Failed to load header</h1>",
+  },
+  props: {},
 });
 
-registerApplication({
-  name: "@skymfe/flight-booking-mf",
-  app: () =>
-    import(
-      /* webpackIgnore: true */ // @ts-ignore-next
-      "@skymfe/flight-booking-mf"
-    ),
-  activeWhen: ["/flights"],
+const applications = constructApplications({
+  routes,
+  loadApp: ({ name }) => import(/* webpackIgnore: true */ name),
 });
 
-start({
-  urlRerouteOnly: true,
+const layoutEngine = constructLayoutEngine({
+  routes,
+  applications,
+  active: false,
 });
+
+applications.forEach(registerApplication);
+
+layoutEngine.activate();
+start();
